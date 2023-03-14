85599ad26fdc5d481abfaa826109dc4f48ca8f63
Christhino <mintsaniaina.christhino@gmail.com>
2023-03-14 20:16:00 +0300

    fix route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "assets/css/App.css";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import AuthLayout from "layouts/auth";
 import AdminLayout from "layouts/admin";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "theme/theme";
